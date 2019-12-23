8a7a1542cadcdca083a9e4aa8823ab6627897aa1
Aditya Dutt <46036968+AdityaDutt@users.noreply.github.com>
2019-12-22 23:18:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 import setuptools
 from setuptools import find_packages
 from distutils.core import setup
+import Testing
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='HWKit',
+    name='Test1',
     author = 'Aditya Dutt',
     author_email = 'aditya.dutt@ufl.edu',
     license='MIT',
-    description="A python library to do segmentation of cursive handwritten words",
+    description="Testing...",
     long_description=long_description,    
     long_description_content_type="text/markdown",    
     version='0.0.1',
     requires = ["setuptools", "wheel"],
     include_package_data=True,
 
-    install_requires=['numpy','opencv-python','scipy','matplotlib','imutils','scikit_image'],
+    install_requires=['numpy'],
     python_requires='>=3.7.0',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -37,7 +38,6 @@ setup(
         'Programming Language :: Python :: 3.6',
 
     ],
-        keywords='python off-line english cursive handwriting handwritten-words character segmentation lexicon-based image recognition image-processing pypi',
     entry_points={  # Optional
         'console_scripts': [
             'sample=sample:main',
