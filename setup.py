72deb2f0edf8187a96c70bcda8f34e0d0d206609
Aditya Dutt <46036968+AdityaDutt@users.noreply.github.com>
2019-12-22 23:31:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="Test-ADT", # Replace with your own username
+setup(
+    name="TestADT", # Replace with your own username
     version="0.0.1",
     author="ADutt",
     author_email="author@example.com",
@@ -12,6 +12,19 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/AdityaDutt/Test",
+    package_dir={'': 'Testing'},  # Optional
+
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(where='Testing'),  # Required
+
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
